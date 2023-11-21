fc3e92e7cc6d3bf3a000255cfe02225807d922b5
Jonathan Guiang <jkguiang@gmail.com>
2023-11-21 15:27:56 -0500

    renamed some files to make it more consistent with /pixels

--- ntuple/main.cc
+++ ntuple/main.cc
@@ -4,8 +4,13 @@
 #include "trkcore.h"
 #include "rooutil.h"
 #include "cxxopts.h"
+#include "tqdm.h"
 
-const float GNNCUT = 0.999;
+const float GNNCUT = 0.99;
+
+struct Node;
+struct Track;
+class Graph;
 
 struct Node
 {
@@ -15,12 +20,13 @@ struct Node
     float pt;
     float eta;
     float phi;
+    float hit0_r_sq;
     std::vector<int> hitidxs;
     std::vector<int> hittypes;
     // Other useful info
-    std::set<Node*> neighbors;    // vector of nodes where (this node) --> (neighbor node)
+    std::set<Node*> neighbors;           // vector of nodes where (this node) --> (neighbor node)
     std::map<Node*, float> edge_scores;  // mapping of GNN scores where (neighbor node) --> edge score
-    int graph_i;                         // index of subgraph that contains this node
+    Graph* graph;                        // pointer to subgraph that contains this node
     bool is_root;                        // false if (another node) --> (this node)
 
     Node(unsigned int node_i)
@@ -32,6 +38,10 @@ struct Node
             pt = lst.pLS_pt().at(lst_idx);
             eta = lst.pLS_eta().at(lst_idx);
             phi = lst.pLS_phi().at(lst_idx);
+            float hit0_x = lst.pLS_0_x().at(lst_idx);
+            float hit0_y = lst.pLS_0_y().at(lst_idx);
+            float hit0_z = lst.pLS_0_z().at(lst_idx);
+            hit0_r_sq = hit0_x*hit0_x + hit0_y*hit0_y + hit0_z*hit0_z;
             hitidxs = lst.pLS_trk_hitidxs().at(lst_idx);
             hittypes = lst.pLS_trk_hittypes().at(lst_idx);
         }
@@ -40,11 +50,15 @@ struct Node
             pt = lst.LS_pt().at(lst_idx);
             eta = lst.LS_eta().at(lst_idx);
             phi = lst.LS_phi().at(lst_idx);
+            float hit0_x = lst.LS_0_x().at(lst_idx);
+            float hit0_y = lst.LS_0_y().at(lst_idx);
+            float hit0_z = lst.LS_0_z().at(lst_idx);
+            hit0_r_sq = hit0_x*hit0_x + hit0_y*hit0_y + hit0_z*hit0_z;
             hitidxs = lst.LS_trk_hitidxs().at(lst_idx);
             hittypes = lst.LS_trk_hittypes().at(lst_idx);
         }
 
-        graph_i = -999;
+        graph = nullptr;
         is_root = true;
     };
 
@@ -88,13 +102,8 @@ struct Track
 
     void addNode(Node* node)
     {
-        pt_sum += node->pt;
 
-        type += 1;
-        if (node->is_pixel)
-        {
-            type += 100;
-        }
+        type += (node->is_pixel) ? 100 : 1;
 
         for (unsigned int hit_i = 0; hit_i < node->hitidxs.size(); ++hit_i)
         {
@@ -103,6 +112,7 @@ struct Track
             unique_hittypes[hitidx] = node->hittypes.at(hit_i);
         }
 
+        pt_sum += node->pt;
         n_nodes++;
         pt = pt_sum/n_nodes;
     };
@@ -122,7 +132,6 @@ struct Track
 class Graph
 {
 private:
-    std::vector<Node*> nodes;
     std::vector<Track*> tracks;
 
     void getTracksRecursive(Node* node, Track* track)
@@ -144,11 +153,24 @@ private:
         }
     };
 public:
+    std::vector<Node*> nodes;
+
     Graph() { /* Do nothing */ };
 
     void addNode(Node* node)
     {
         nodes.push_back(node);
+    };
+    
+    void addGraph(Graph* graph)
+    {
+        nodes.insert(nodes.end(), graph->nodes.begin(), graph->nodes.end());
+    };
+
+    void clear()
+    {
+        nodes.clear();
+        tracks.clear();
     };
 
     std::vector<Track*> getTracks()
@@ -174,6 +196,36 @@ public:
         );
         return tracks;
     };
+};
+
+std::pair<Node*, Node*> sortNodes(Node* node0, Node* node1)
+{
+    if (node0->is_pixel && !node1->is_pixel)
+    {
+        // Node0 is a pixel node, Node1 is not, so Node0 is closer to the beamline
+        return std::pair<Node*, Node*>(node0, node1);
+    }
+    else if (!node0->is_pixel && node1->is_pixel)
+    {
+        // Node1 is a pixel node, Node0 is not, so Node1 is closer to the beamline
+        return std::pair<Node*, Node*>(node1, node0);
+    }
+    else
+    {
+        // The two nodes are the same kind of node, so we need to compare r^2
+        if (node0->hit0_r_sq < node1->hit0_r_sq)
+        {
+            return std::pair<Node*, Node*>(node0, node1);
+        }
+        else if (node0->hit0_r_sq > node1->hit0_r_sq)
+        {
+            return std::pair<Node*, Node*>(node1, node0);
+        }
+        else
+        {
+            throw std::runtime_error("Error - found two nodes with the same hit0_r_sq");
+        }
+    }
 };
 
 // ./process INPUTFILEPATH OUTPUTFILE [NEVENTS]
@@ -206,12 +258,14 @@ int main(int argc, char** argv)
     gnn_looper.init(gnnNtuple_tchain, &gnn, -1); 
 
     /* --- START: event loop --- */
+    tqdm bar;
     unsigned int n_events = gnn_looper.getNEventsTotalInChain();
     for (unsigned int event_i = 0; event_i < n_events; ++event_i)
     {
         lst_looper.nextEvent();
         trk_looper.nextEvent();
         gnn_looper.nextEvent();
+        bar.progress(event_i, n_events);
 
         /* --- START: collect all connected components (isolated subgraphs) --- */
         std::vector<Node*> nodes;
@@ -226,37 +280,59 @@ int main(int argc, char** argv)
             float edge_score = gnn.Edge_score().at(edge_i);
             if (edge_score > GNNCUT)
             {
-                Node* node0 = nodes.at(gnn.Edge_node0Idx().at(edge_i));
-                Node* node1 = nodes.at(gnn.Edge_node1Idx().at(edge_i));
+                std::pair<Node*, Node*> edge_nodes = sortNodes(
+                    nodes.at(gnn.Edge_node0Idx().at(edge_i)),
+                    nodes.at(gnn.Edge_node1Idx().at(edge_i))
+                );
+                Node* node0 = edge_nodes.first;
+                Node* node1 = edge_nodes.second;
                 node0->addNeighbor(node1, edge_score);
                 node1->is_root = false;
 
-                bool node0_in_subgraph = (node0->graph_i >= 0);
-                bool node1_in_subgraph = (node1->graph_i >= 0);
+                bool node0_in_subgraph = (node0->graph != nullptr);
+                bool node1_in_subgraph = (node1->graph != nullptr);
 
                 if (node0_in_subgraph && node1_in_subgraph)
                 {
-                    continue;
+                    if (node0->graph != node1->graph)
+                    {
+                        // Merge Node1's graph into Node0's graph
+                        node0->graph->addGraph(node1->graph);
+                        /* NOTE: we clear Node1's original graph without deleting it 
+                         *       because it is not worth spending the time to find 
+                         *       and delete it. */
+                        node1->graph->clear();
+                        // Reset the graph index for all nodes in the merged graph
+                        for (auto* node : node0->graph->nodes)
+                        {
+                            node->graph = node0->graph;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 }
                 else if (node0_in_subgraph && !node1_in_subgraph)
                 {
-                    Graph* graph = subgraphs.at(node0->graph_i);
-                    graph->addNode(node1);
-                    node1->graph_i = node0->graph_i;
+                    // Add Node1 to Node0's graph
+                    node0->graph->addNode(node1);
+                    node1->graph = node0->graph;
                 }
-                else if (!node0_in_subgraph&& node1_in_subgraph)
+                else if (!node0_in_subgraph && node1_in_subgraph)
                 {
-                    Graph* graph = subgraphs.at(node1->graph_i);
-                    graph->addNode(node0);
-                    node0->graph_i = node1->graph_i;
+                    // Add Node0 to Node1's graph
+                    node1->graph->addNode(node0);
+                    node0->graph = node1->graph;
                 }
                 else
                 {
+                    // Add Node0 and Node1 to a new graph
                     Graph* graph = new Graph();
                     graph->addNode(node0);
                     graph->addNode(node1);
-                    node0->graph_i = subgraphs.size();
-                    node1->graph_i = subgraphs.size();
+                    node0->graph = graph;
+                    node1->graph = graph;
                     subgraphs.push_back(graph);
                 }
             }
@@ -270,8 +346,7 @@ int main(int argc, char** argv)
         {
             /* TODO: this finds all tracks in a subgraph, then saves only up to three 
              *       tracks with the largest GNN score sums. This is not a good metric, 
-             *       but it is a start. Improve this in the future!
-             */
+             *       but it is a start. Improve this in the future! */
             // Perform depth-first search to find all paths (track candidates)
             std::vector<Track*> tracks = graph->getTracksSorted();
             // Process track candidates
@@ -320,6 +395,8 @@ int main(int argc, char** argv)
         tx.fill();
         /* --- END: wrap up this event --- */
     }
+
+    bar.finish();
 
     tfile->cd();
     tx.write();
